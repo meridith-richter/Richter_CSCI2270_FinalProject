6026911c077df13493add747f14c7f456ead4a1b
meridith-richter <meri4965@colorado.edu>
2016-04-28 02:57:12 -0600

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,106 +1,12 @@
 #include <iostream>
-#include "HashTable.h"
+#include "LinkedList.h"
+
 
 using namespace std;
 
-#include "HashTable.h"
-
-//LinkedList Class implementation
-LinkedList::LinkedList(){
-    head = new Ingredient;
-    head->next = NULL;
-    length = 0;
-}
-
-// Inserts an item (end of list)
-void LinkedList::insertItem(Ingredient *newItem){
-    if(!head -> next){
-        head -> next = newItem;
-        length++;
-        return;
-    }
-    Ingredient *temp = head;
-    Ingredient *cursor = head;
-    while(cursor){
-        temp = cursor;
-        cursor = temp->next;
-    }
-    temp->next = newItem;
-    newItem->next = NULL;
-    length++;
-}
-
-bool LinkedList::removeItem(string itemName)
-{
-    if(!head->next){
-        return false;
-    }
-    Ingredient *temp = head;
-    Ingredient *cursor = head;
-    while(cursor){
-        if(cursor->name == itemName){
-            temp->next = cursor->next;
-            delete cursor;
-            length--;
-            return true;
-        }
-        temp = cursor;
-        cursor = temp->next;
-    }
-    return false;
-}
-
-// Searches for an item by its key.
-// Returns a reference to first match.
-// Returns a NULL pointer if no match is found.
-Ingredient *LinkedList::getItem(string itemName){
-    Ingredient *temp = head;
-    Ingredient *cursor = head;
-    while(cursor){
-        temp = cursor;
-        if((temp != head) && (temp->name == itemName))
-            return temp;
-        cursor = temp->next;
-    }
-    return NULL;
-}
-
-void LinkedList::printList(int index){
-    if(length == 0){
-        //cout <<"empty"<<endl;
-        return;
-    }
-    Ingredient *temp = head;
-    Ingredient *cursor = head;
-    while(cursor){
-        temp = cursor;
-        if(temp != head){
-            cout<<index<<":";
-            cout <<temp->name<<":";
-            if(temp-> next){
-                cout <<endl;
-            }
-        }
-        cursor = temp->next;
-    }
-    cout<<endl;
-}
-
-int LinkedList::getLength(){
-    return length;
-}
-
-LinkedList::~LinkedList(){
-    Ingredient * temp = head;
-    Ingredient * cursor = head;
-    while (cursor){
-        temp = cursor;
-        cursor = temp->next;
-        if(cursor){
-            delete temp;
-        }
-    }
-}
+//alphabet arrays for classification
+char UPalphaArr[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+char LOWalphaArr[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
 //HashTable class implementation
 HashTable::HashTable(int tableLength){
@@ -114,13 +20,18 @@ HashTable::HashTable(int tableLength){
 //dictates hash value
 int HashTable::hash(string itemName){
     int value = 0;
-    for(int i = 0; i < itemName.length(); i++){
-        value += itemName[i];
+    for(int i = 0; i < 26; i++){
+        if(itemName[1] == UPalphaArr[i] || itemName[1] == LOWalphaArr[i]){
+            value = i + 1;
+        }
     }
-    return value % 26;
+    //cout<<itemName<<" ";
+    //cout<<value<<" ";
+    return value;
 }
 void HashTable::insertItem(Ingredient *newItem){
     int index = hash(newItem->name);
+    //cout<<newItem->classification<<endl;
     array[index].insertItem(newItem);
 }
 
@@ -130,31 +41,34 @@ bool HashTable::removeItem(string itemName){
     return array[index].removeItem(itemName);
 }
 
-Ingredient *HashTable::getItemByKey(string itemName){
+Ingredient *HashTable::getItemByName(string itemName){
     int index = hash(itemName);
     return array[index].getItem(itemName);
 }
 
-
 void HashTable::printTable(){
-    //cout << "\n\nHash Table:\n";
     for(int i = 0; i < length; i++){
-        //cout<< i + 1 << ":";
         array[i].printList(i + 1);
     }
 }
 
+Ingredient *HashTable::returnItem(int index){
+    string name = array[index].returnHead();
+    Ingredient *item = getItemByName(name);
+    return item;
+}
+
 //histogram
 void HashTable::printHistogram(){
-    cout << "\n\nHash Table Contains ";
-    cout << getNumberOfItems() << " Items total\n";
+    cout<<endl;
+    cout<<"Table Contains ";
+    cout<<getNumberOfItems()<<" Items:"<<endl;
     for(int i = 0; i < length; i++){
-        cout << i + 1 << ":\t";
-        for(int j = 0; j < array[i].getLength(); j++){
-            cout << " X";
-        }
-        cout << "\n";
+        cout<<i + 1<<": ";
+        array[i].histogramPrintList(i);
+        cout<<endl;
     }
+    cout<<endl;
 }
 
 int HashTable::getLength(){
