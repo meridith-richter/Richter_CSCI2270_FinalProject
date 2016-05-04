06561af26d86588d03b646ed25bc658ca1462707
meridith-richter <meri4965@colorado.edu>
2016-05-04 17:33:54 -0600

    Update HashTable.cpp
    most recent

--- HashTable.cpp
+++ HashTable.cpp
@@ -2,8 +2,13 @@
 #include "LinkedList.h"
 #include "HashTable.h"
 
-
 using namespace std;
+
+//int for counter check
+int index = 0;
+
+//additional index
+int i = 0;
 
 //alphabet arrays for classification
 char UPalphaArr[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
@@ -18,16 +23,14 @@ HashTable::HashTable(int tableLength){
     length = tableLength;
 }
 
-//dictates hash value
+//dictates hash value, will be organized alphabetically
 int HashTable::hash(string itemName){
     int value = 0;
     for(int i = 0; i < 26; i++){
         if(itemName[1] == UPalphaArr[i] || itemName[1] == LOWalphaArr[i]){
-            value = i + 1;
+            value = i;
         }
     }
-    //cout<<itemName<<" ";
-    //cout<<value<<" ";
     return value;
 }
 void HashTable::insertItem(Ingredient *newItem){
@@ -49,14 +52,36 @@ Ingredient *HashTable::getItemByName(string itemName){
 
 void HashTable::printTable(){
     for(int i = 0; i < length; i++){
-        array[i].printList(i + 1);
+        array[i].printList(i);
     }
 }
 
-Ingredient *HashTable::returnItem(int index){
-    string name = array[index].returnHead();
-    Ingredient *item = getItemByName(name);
-    return item;
+Ingredient *HashTable::getList(int index){
+    Ingredient *newHead = array[index].returnHead(index);
+    return newHead;
+}
+
+//function to compare each item of the user's file which is in the same alphabetical category
+//of the bad ingredients list to those ingredients with the same alphabetical classification
+void HashTable::compare(Ingredient *userHead, Ingredient *fileHead){
+    Ingredient *temp = userHead;
+    Ingredient *compTemp = fileHead;
+    //cout<<temp->name<<endl;
+    while(temp != NULL){
+        //cout<<temp->name<<endl;
+        compTemp = fileHead;
+        while(compTemp != NULL){
+            //cout<<compTemp->name<<endl;
+            //cout<<"Comparing..."<<temp->name<<" to"<<compTemp->name<<endl;
+            if(compTemp->name == temp->name){
+                cout<<"MATCH: WARNING contains"<<compTemp->name<<endl;
+                strArr[i] = compTemp->name;
+                i++;
+            }
+            compTemp = compTemp->next;
+        }
+        temp = temp->next;
+    }
 }
 
 //histogram
@@ -84,6 +109,15 @@ int HashTable::getNumberOfItems(){
     return itemCount;
 }
 
+void HashTable::setUpstrArr(){
+    //initialize string array
+    for(int j = 0; j < 30; j++){
+        strArr[j] = "-1";
+    }
+}
+
 HashTable::~HashTable(){
     delete []array;
 }
+
+
